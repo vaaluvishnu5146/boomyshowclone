9d5d7daa7da24830b72a8f987ecaa73c2029e7c7
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-09-20 20:59:17 +0530

    Typo fix

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 console.log(process.env.NODE_ENVIRONMENT);
 
 async function init() {
-  const URI = `${process.env.NODE_DATABASE_CONNECTION_STRING_DEV}/${process.env.NODE_DATABASE_NAME}`;
+  let URI = `${process.env.NODE_DATABASE_CONNECTION_STRING_DEV}/${process.env.NODE_DATABASE_NAME}`;
   if (process.env.NODE_ENVIRONMENT === "production") {
     URI = `mongodb+srv://${process.env.NODE_MONGODB_USERNAME}:${process.env.NODE_MONGODB_PASSWORD}@cluster0.ez75xfb.mongodb.net/${process.env.NODE_DATABASE_NAME}`;
   }
